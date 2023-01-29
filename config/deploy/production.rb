1199a0abfd25e1465ac6c7d53102377901c8a2b7
Justin Ye <hi_justin@hotmail.com>
2023-01-30 09:51:42 +1100

    correct deploy website

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server 'system_classroom.com', user: 'deploy', roles: %w{app db web}
+server 'systemclassroom.com', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
