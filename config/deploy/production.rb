2c905512d4b5973ae7b06e227b7cf1b2e97f5f9c
Justin Ye <hi_justin@hotmail.com>
2023-01-30 23:43:07 +1100

    newuser

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server 'systemclassroom.com', user: 'deploy', roles: %w{app db web}
+server 'systemclassroom.com', user: 'justinye', roles: %w{app db web}
 
 # role-based syntax
 # ==================
