e77fe5584e4787a673d2dd122b897eff582d5c05
Justin Ye <hi_justin@hotmail.com>
2023-01-31 11:46:45 +1100

    remove users

--- db/migrate/20221215044732_remove_references_from_user.rb
+++
@@ -1,6 +0,0 @@
-class RemoveReferencesFromUser < ActiveRecord::Migration[7.0]
-    def change
-      remove_reference :users, :creator, polymorphic: true
-      remove_reference :users, :member, polymorphic: true
-    end
-end
--- db/migrate/20221215045200_add_loginable_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddLoginableToUser < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :users, :loginable, polymorphic: true
-  end
-end
