fd668d616c304b08259c7e4697f796e0f6325b5c
Justin Ye <hi_justin@hotmail.com>
2023-02-08 14:50:47 +1100

    log easy way

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,9 +85,9 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
-    #logger           = ActiveSupport::Logger.new()
-    #logger.formatter = config.log_formatter
-    config.logger    =  Logger.new("log/production.log")
+    logger           = ActiveSupport::Logger.new("log/#{Rails.env}.log")
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
   # Do not dump schema after migrations.
