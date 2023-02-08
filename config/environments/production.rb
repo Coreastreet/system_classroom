e5f07fac128deb74f6014375bb8197a724097f90
Justin Ye <hi_justin@hotmail.com>
2023-02-08 13:41:57 +1100

    log file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    config.logger    = ActiveSupport::TaggedLogging.new("log/#{Rails.env}.log")
   end
 
   # Do not dump schema after migrations.
